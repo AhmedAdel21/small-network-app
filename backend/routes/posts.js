1c8f1043c06f7a55194e3db1c16701de3dfe031e
AhmedAdel21 <ahmed12365488@gmail.com>
2026-01-04 22:44:27 +0200

    add multer to server side and prepare the server to upload images

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,8 +1,32 @@
 const Post = require("../models/post");
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
-router.post("", async (req, res, next) => {
+const MIME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpg",
+  "image/jpg": "jpg",
+};
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "backend/uploads/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(" ").join("-");
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + "-" + Date.now() + "." + ext);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post("", upload.single("image"), async (req, res, next) => {
   console.log("post request received");
   const post = new Post({
     title: req.body.title,
